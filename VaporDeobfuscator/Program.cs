41882d1a08ed03517db1a46383446ff2908a1093
minhara. <ivanovskiy.davidik@gmail.com>
2026-01-23 00:00:43 +0200

    Add files via upload

---
+++ VaporDeobfuscator/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace VaporDeobfuscator
+{
+    class Program
+    {
+        static string[] attrib =
+        {
+            "YanoAttribute",
+            "Xenocode.Client.Attributes.AssemblyAttributes.ProcessedByXenocode",
+            "PoweredByAttribute",
+            "ObfuscatedByGoliath",
+            "NineRays.Obfuscator.Evaluation",
+            "NetGuard",
+            "dotNetProtector",
+            "DotNetPatcherPackerAttribute",
+            "DotNetPatcherObfuscatorAttribute",
+            "DotfuscatorAttribute",
+            "CryptoObfuscator.ProtectedWithCryptoObfuscatorAttribute",
+            "BabelObfuscatorAttribute",
+            "BabelAttribute",
+            "AssemblyInfoAttribute"
+        };
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) return;
+
+            var module = ModuleDefMD.Load(args[0]);
+
+            FixModuleName(module, args[0]);
+            RemoveFakeTypes(module);
+            RemoveAntiDe4Dot(module);
+            DecryptStrings(module);
+
+            var outPath = Path.Combine(
+                Path.GetDirectoryName(args[0]),
+                Path.GetFileNameWithoutExtension(args[0]) + ".unpacked" + Path.GetExtension(args[0])
+            );
+
+            module.Write(outPath);
+        }
+
+        static void FixModuleName(ModuleDefMD module, string path)
+        {
+            if (module.Name == "ObfuscatedByVapor")
+                module.Name = Path.GetFileName(path);
+        }
+
+        static void RemoveFakeTypes(ModuleDefMD module)
+        {
+            var list = module.Types.Where(t =>
+                attrib.Any(a => t.Name.String == a) ||
+                t.Name.String.Contains("俺ム仮") ||
+                t.Namespace.String.Contains("俺ム仮")
+            ).ToList();
+
+            foreach (var t in list)
+                module.Types.Remove(t);
+        }
+
+        static void RemoveAntiDe4Dot(ModuleDefMD module)
+        {
+            var list = module.Types.Where(t =>
+                t.Name.String.StartsWith("Form") &&
+                t.Interfaces.Count > 0 &&
+                t.BaseType != null &&
+                t.BaseType.FullName == "System.Attribute"
+            ).ToList();
+
+            foreach (var t in list)
+                module.Types.Remove(t);
+        }
+
+        static void DecryptStrings(ModuleDefMD module)
+        {
+            foreach (var type in module.Types)
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (!method.HasBody) continue;
+
+                    var body = method.Body;
+                    var ins = body.Instructions;
+
+                    for (int i = 0; i < ins.Count - 5; i++)
+                    {
+                        if (ins[i].OpCode == OpCodes.Nop &&
+                            ins[i + 1].OpCode == OpCodes.Call &&
+                            ins[i + 2].OpCode == OpCodes.Ldstr &&
+                            ins[i + 3].OpCode == OpCodes.Call &&
+                            ins[i + 4].OpCode == OpCodes.Callvirt)
+                        {
+                            try
+                            {
+                                string b64 = ins[i + 2].Operand.ToString();
+                                string s = Encoding.UTF8.GetString(Convert.FromBase64String(b64));
+
+                                ins[i].OpCode = OpCodes.Ldstr;
+                                ins[i].Operand = s;
+
+                                for (int j = 1; j <= 4; j++)
+                                {
+                                    ins[i + j].OpCode = OpCodes.Nop;
+                                    ins[i + j].Operand = null;
+                                }
+                            }
+                            catch { }
+                        }
+                    }
+
+                    body.OptimizeBranches();
+                    body.OptimizeMacros();
+                }
+            }
+        }
+    }
+}
---
+++ VaporDeobfuscator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VaporDeobfuscator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VaporDeobfuscator")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dab1ca88-4fc0-4d0e-b2d5-456755c988fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
